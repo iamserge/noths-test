f7273c87c06bd393ae8bd1cbfe8d5dd85792a5bc
sradkevics <sradkevics@wundermancommerce.com>
2020-09-22 13:38:49 +0100

    More tests for GildedRose class

--- tests/gilded-rose.test.ts
+++ tests/gilded-rose.test.ts
@@ -61,14 +61,93 @@ describe('GildedRose', function () {
         const GildedRoseInventory = new GildedRose(items, rules)
         expect(GildedRoseInventory.rules).to.equal(rules);
     });
-    it('should update items correctly', function() {
+    it('should update regular item correctly', function() {
         const items = [
-            new Item('Aged Brie', 10, 20),
             new Item('+5 Dexterity Vest', 10, 20),
         ]; 
         const GildedRoseInventory = new GildedRose(items, rules)
         GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(19);
+    });
+    it('should update "Aged Brie" correctly', function() {
+        const items = [
+            new Item('Aged Brie', 10, 20),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
         expect(GildedRoseInventory.items[0].quality).to.equal(21);
+    });
+    it('should update regular item passed sell in date correctly', function() {
+        const items = [
+            new Item('+5 Dexterity Vest', 0, 20),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(18);
+    });
+    it('should update backstage pass correctly', function() {
+        const items = [
+            new Item('Backstage passes 1', 20, 20),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(21);
+    });
+    it('should update backstage pass correctly with less than 10 days', function() {
+        const items = [
+            new Item('Backstage passes 2', 8, 20),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(22);
+    });
+    it('should update backstage pass correctly with less than 5 days', function() {
+        const items = [
+            new Item('Backstage passes 3', 4, 20),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(23);
+    });
+    it('should update backstage pass correctly with 0 days', function() {
+        const items = [
+            new Item('Backstage passes 3', 0, 20),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(0);
+    });
+    it('should set minimum limit for quality to 0', function() {
+        const items = [
+            new Item('+5 Dexterity Vest', 1, 0),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(0);
+    });
+    it('should update Conjured items correctly', function() {
+        const items = [
+            new Item('Conjured 1', 10, 20),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(18);
+    });
+    it('should set maximum limit for quality to 50', function() {
+        const items = [
+            new Item('Backstage passes', 10, 50),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(50);
+    });
+    it('should not update quality for Sulfuras items', function() {
+        const items = [
+            new Item('Sulfuras 1', 10, 80),
+        ]; 
+        const GildedRoseInventory = new GildedRose(items, rules)
+        GildedRoseInventory.updateQuality();
+        expect(GildedRoseInventory.items[0].quality).to.equal(80);
     });
 });
 
